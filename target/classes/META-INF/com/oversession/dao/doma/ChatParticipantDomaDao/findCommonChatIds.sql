ed6a2bfa2c094da1588e293ed55723668f828f67
koichi <2020m086@kuas.ac.jp>
2026-04-04 14:54:24 +0900

    docker 追加

--- target/classes/META-INF/com/oversession/dao/doma/ChatParticipantDomaDao/findCommonChatIds.sql
+++
@@ -1,6 +0,0 @@
-SELECT cp1.chat_id
-FROM chat_participants cp1
-JOIN chat_participants cp2
-    ON cp1.chat_id = cp2.chat_id
-WHERE cp1.user_id = /* userId1 */'user1'
-  AND cp2.user_id = /* userId2 */'user2'
--- target/classes/META-INF/com/oversession/dao/doma/UserDomaDao/searchByKeyword.sql
+++
@@ -1,9 +0,0 @@
-SELECT /*%expand*/*
-FROM users
-WHERE deleted = false
-  AND (
-    LOWER(user_name)                LIKE /* @infix(keyword) */'%test%'
-    OR LOWER(department_name)       LIKE /* @infix(keyword) */'%test%'
-    OR LOWER(primary_head_office_name) LIKE /* @infix(keyword) */'%test%'
-  )
-ORDER BY user_id
